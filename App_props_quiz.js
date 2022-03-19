feb489e236d827da3a4355dd3e8b21f83f676559
hyoungwonkang <hwplus@gmail.com>
2022-03-19 18:00:56 +0900

    리액트 기초반 퀴즈1. props 쓰기

---
+++ App_props_quiz.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./App.css"
+import BucketList from './BucketList';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      list: ['훌륭한 개발자 되기','여행 가기','영어 잘하기'],
+    }
+  }
+
+  render() {        // DidUpdate, DidMount는 없어도 되지만 render는 필수다. JSX의 규칙. 리턴되는값은 있어야 한다.
+    console.log(this.state.list)
+    return (
+      <div className='App'>
+        <h1>내 버킷리스트</h1>
+        <BucketList list = {this.state.list}/>
+      </div>
+    )
+  }
+
+}
+
+ 
+
+export default App;
