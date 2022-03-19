115c2a2b4669c1c6f88f81c9e3315494334b049a
hyoungwonkang <hwplus@gmail.com>
2022-03-19 15:13:15 +0900

    리액트 기초반 2-2 생성, 업데이트, 제거 라이프사이클 연습

---
+++ App_import_LifecycleEx_class_and_function.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import LifecycleEx from './LifecycleEx';
+import React from "react";
+
+function App() {
+  const [is_cat, setIsCat] = React.useState(true);
+  return (
+      <div className="App">
+          {is_cat ? <LifecycleEx/> : null}
+          <button onClick={()=> {setIsCat(!is_cat)}}>바꾸기</button>
+      </div>
+  )            // React Hooks 이라는 함수
+  return (
+    <div className="App">
+      <LifecycleEx/>
+    </div>
+  );
+}
+
+export default App;
