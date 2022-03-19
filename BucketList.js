4d5581e1c62eec8b988a6c1ede3e3bbe798ceeb3
hyoungwonkang <hwplus@gmail.com>
2022-03-19 16:07:18 +0900

    리액트 기초반 2-3 함수형 컴포넌트 연습

---
+++ BucketList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BucketList = (props) =>{      //props는 파라미터
+
+    return (
+        <div>버킷리스트</div>
+    )
+}
+
+export {BucketList}
+// export default BucketList
